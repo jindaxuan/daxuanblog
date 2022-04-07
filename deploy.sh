8923c964abbac25fd6f98334fb078ad532f0eacc
金煊 <553038034@qq.com>
2022-04-08 05:36:57 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
  
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/jindaxuan/daxuanblog master
+git push -f git@github.com:jindaxuan/daxuanblog.git master
  
 cd -
